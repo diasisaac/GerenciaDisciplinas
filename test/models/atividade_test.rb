5b1a639b018881a3aa31067183308b9abeebd3df
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-24 03:20:57 -0300

    2 testes de model atividade_test.rb

--- test/models/atividade_test.rb
+++ test/models/atividade_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class AtividadeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Criar atividade corretamente" do
+    turma = Turma.new nome:"3A",
+                     ano_letivo:"2022"
+    turma.save
+
+    disciplina = Disciplina.new nome:'Filosofia',
+                                ano_letivo:'2022',
+                                turma_id:turma.id
+    disciplina.save
+
+    atividade = Atividade.new titulo:"Refletir",
+                              descricao:"Reflita a filosofia de Engenharia de Software",
+                              bimestre:"1",
+                              data:"2022-06-30",
+                              disciplina_id:disciplina.id
+    assert atividade.save
+  end
+
+  test "Criar atividade sem data" do
+    turma = Turma.new nome:"3A",
+                      ano_letivo:"2022"
+    turma.save
+
+    disciplina = Disciplina.new nome:'Filosofia',
+                                ano_letivo:'2022',
+                                turma_id:turma.id
+    disciplina.save
+
+    atividade = Atividade.new titulo:"Refletir",
+                              descricao:"Reflita a filosofia de Engenharia de Software",
+                              bimestre:"1",
+                              disciplina_id:disciplina.id
+    assert_not atividade.save
+  end
 end
