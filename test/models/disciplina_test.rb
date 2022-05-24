143491f96a9b2c5a81e484db849a967cf70bc500
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-23 23:10:42 -0300

    Implementacao de testes em model disciplina_test.rb

--- test/models/disciplina_test.rb
+++ test/models/disciplina_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class DisciplinaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Criar disciplina corretamente" do
+    turma = Turma.new nome: '8D',
+                      ano_letivo: '2022'
+
+    assert turma.save
+
+    disciplina = Disciplina.new nome:'Matematica',
+                                ano_letivo:'2023',
+                                turma_id: turma.id
+    assert disciplina.save
+  end
+
+  test "Criar disciplina sem campo nome" do
+    turma = Turma.new nome: '8D',
+                      ano_letivo: '2022'
+
+    assert turma.save
+
+    disciplina = Disciplina.new ano_letivo:'2023',
+                                turma_id: turma.id
+    assert_not disciplina.save
+  end
 end
