c430fbf809dc9bc6a1ab550ef045e44490c1d9c0
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-24 08:26:30 -0300

    Atualizacao de testes model avaliacao

--- test/models/avaliacao_test.rb
+++ test/models/avaliacao_test.rb
@@ -28,4 +28,31 @@ class AvaliacaoTest < ActiveSupport::TestCase
                                   aluno_id:aluno.id
         assert avaliacao.save
     end
+    test "Nao deve criar avaliação sem aluno" do
+        turma = Turma.new nome:"5A",
+                          ano_letivo:"2022"
+        turma.save
+
+        disciplina = Disciplina.new nome:'Inglrs',
+                                    ano_letivo:'2022',
+                                    turma_id:turma.id
+        disciplina.save
+
+        aluno= Aluno.new nome:"João Paulo",
+                         turma_id:turma.id
+        aluno.save
+
+        atividade = Atividade.new titulo:"Verbos",
+                                  descricao:"Vai ser dificil",
+                                  bimestre:"1",
+                                  data:"2022-10-10",
+                                  disciplina_id:disciplina.id
+        atividade.save
+
+        avaliacao = Avaliacao.new pontos: '5',
+                                  observacoes:"Errou muito",
+                                  atividade_id:atividade.id
+
+        assert_not avaliacao.save
+    end
 end
