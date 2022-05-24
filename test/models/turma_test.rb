091e27a526eedca0764c751151eee02f52fc4164
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-23 21:19:35 -0300

    2 testes de model turma

--- test/models/turma_test.rb
+++ test/models/turma_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class TurmaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve salvar Turma corretamente" do
+    turma = Turma.new  nome: "8A",
+                        ano_letivo: "2025"
+
+    assert turma.save
+  end
+  test "Nao deve salvar Turma sem nome" do
+    turma = Turma.new ano_letivo: "2025"
+
+    assert_not turma.save
+  end
 end
