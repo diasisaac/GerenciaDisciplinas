942bb66ec98004cefad812e8fa4638122d705fa0
Isaac Dias <m.dias.isaac@gmail.com>
2022-05-23 23:07:37 -0300

    Implementacao de testes em aluno_test.rb

--- test/models/aluno_test.rb
+++ test/models/aluno_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class AlunoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve salvar Aluno corretamente" do
+    turma = Turma.new nome: '8D',
+                            'ano_letivo': 2022
+    turma.save
+
+    aluno = Aluno.new  nome: "8A",
+                       turma: turma
+
+    assert aluno.save
+  end
+
+  test "Nao deve salvar Aluno sem nome" do
+    turma = Turma.new nome: '8D',
+                      'ano_letivo': 2022
+    turma.save
+
+    aluno = Aluno.new turma: turma
+
+    assert_not aluno.save
+  end
 end
